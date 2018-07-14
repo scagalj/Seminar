9095265789e4a2ce0423be0371cd7d16dd45a4be
Stjepan Cagalj <stjepan_12@hotmail.com>
2018-07-14 14:28:44 +0200

    Dodana mogucnost brisanja sobe

--- Seminar1/Resource.h
+++ Seminar1/Resource.h
@@ -44,6 +44,8 @@
 #define IDC_STATIC_POPIS_HID            1036
 #define IDC_STATIC_POPIS_HNAZIV         1037
 #define IDC_LIST_POPIS_SOBA             1038
+#define IDC_BUTTON_POPIS_IZBRISI_SOBU   1039
+#define IDC_BUTTON1                     1040
 
 // Next default values for new objects
 // 
@@ -51,7 +53,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        140
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_CONTROL_VALUE         1041
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
